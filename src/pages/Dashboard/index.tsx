9778e05d92296e06726ca9a8638e2f51b0a53d91
Magno Belloni <bellonidesouza6@gmail.com>
2020-07-15 16:41:58 -0300

    Resolução do desafio.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
 
 import formatValue from '../../utils/formatValue';
 import { useCart } from '../../hooks/cart';
@@ -35,14 +35,15 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const productsReturn = await api.get('products');
+      setProducts(productsReturn.data);
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
@@ -73,6 +74,9 @@ const Dashboard: React.FC = () => {
         />
       </ProductContainer>
       <FloatingCart />
+      {/* <Button title="" }> */}
+
+      {/* </Button> */}
     </Container>
   );
 };
